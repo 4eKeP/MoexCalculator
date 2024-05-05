fd588524612c1704a1676a7bde93d05ab3783a76
Egor <egor_chek@mail.ru>
2024-05-05 12:46:00 +0400

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by admin on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
