84088cf95331ffe4f01e64f2329513f502ca4673
Egor <egor_chek@mail.ru>
2024-05-05 13:50:31 +0400

    реализован интерфейс и логика отображения/пересчета валют

---
+++ MoexCalculator/CurrencyInput.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyInput.swift
+//  MoexCalculator
+//
+//  Created by admin on 05.05.2024.
+//
+
+import SwiftUI
+
+struct CurrencyInput: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    CurrencyInput()
+}
